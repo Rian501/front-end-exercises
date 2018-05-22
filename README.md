01e207b23e80b2ca72dea3a39d1f23010d8c9c3c
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-05-22 10:10:49 -0500

    congressional district backbone laid out, incl challenges, BEM structure shaky

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# front-end-exercises
+# Front End Exercises
+
+This repo is where I will store my own work on exercises found in the [NSS Client Side Mastery course](https://github.com/nashville-software-school/client-side-mastery).
+
+# Book Two Exercises
+1. [Congressional District](https://github.com/nashville-software-school/client-side-mastery/blob/master/book-2-the-novice/chapters/HTML_COMPONENTS.md)
+<!-- Chicken Monkey
+Take a Number -->
+
+# Book Two Challenges
+1. [Congressional District](https://github.com/nashville-software-school/client-side-mastery/blob/master/book-2-the-novice/chapters/HTML_COMPONENTS.md)
---
+++ congressionalDistrict/main.js
@@ -0,0 +1,18 @@
+console.log('Woooooooo, main.js is hooked up!');
+
+
+let repComponent = document.querySelector('#mainRepComponent');
+
+let newSection = document.createElement("section");
+newSection.innerHTML = '<p>Win win win</p>'
+// let missionStatement = document.createTextNode("Win at all costs.");
+// newSection.appendChild(missionStatement);
+
+repComponent.appendChild(newSection);
+
+
+
+//challenge 2
+let customAtt = document.createAttribute("congressional-district"); // Create a custom attribute
+customAtt.value = "5";   //the value is that of my district
+repComponent.setAttributeNode(customAtt); // Add the attribute to the article (made into a variable above)
