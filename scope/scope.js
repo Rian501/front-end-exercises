c119163be6a49b714854a80f8db93b43023049e4
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-05-24 14:25:04 -0500

    scope exercises

--- scope/scope.js
+++ scope/scope.js
@@ -2,70 +2,74 @@
 const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 let y = 1
 
-for (let x = 1; y < cookies.length; x++) {
-  const currentCookie = cookies[y]
+for (let x = 1; x < cookies.length; x++) {
+  const currentCookie = cookies[x]
   console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 }
 
 
-// // Conjunction Function
-// const conjunction = function (firstWord, secondWord) {
-//   const conjoinedWord = `${firstWord} ${secondWord}`
-// }
+// Conjunction Function
+let conjoinedWord;
+const conjunction = function (firstWord, secondWord) {
+  conjoinedWord = `${firstWord} ${secondWord}`
+}
 
-// conjunction("Master", "Card")
-// console.log(conjoinedWord)
-
-
-// // Mod Squad
-// {
-//   const ModSquad = {
-//     "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"]
-//         "series": {
-//       "start": "1968",
-//       "end": "1973"
-//     }
-//   }
-
-//   const HTMLRepresentation = `<h1>The Mod Squad</h1>`
-
-//   ModSquad.members.forEach(member => {
-//     const HTMLRepresentation += `<div>${member}</div>`
-//   })
-// }
-
-// document.querySelector(".show-info").innerHTML = HTMLRepresentation
+conjunction("Master", "Card");
+console.log(conjoinedWord);
 
 
-// // Simon Says
-// const locations = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]
+// Mod Squad
+{
+  const ModSquad = {
+    "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+    "series": {
+      "start": "1968",
+      "end": "1973"
+    }
+  }
 
-// for (let k = 0; k < locations.length; k++) {
-//   const currentLocation = locations[k]
+  let HTMLRepresentation = `<h1>The Mod Squad</h1>`
 
-//   if (currentLocation[0] > 2) {
-//     const invalidLocation = true
-//   }
+  ModSquad.members.forEach(member => {
+    HTMLRepresentation += `<div>${member}</div>`
+  })
+  document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
 
-//   if (invalidLocation) {
-//     console.log("This location is invalid")
-//   }
-// }
 
-// console.log(`There were ${k} locations displayed`)
 
-// // Lambda Llama
-// const llamaNamer = function () {
-//   const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
-//   const randomizer = Math.floor(Math.random() * 7)
+// Simon Says
+const locations = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]
 
-//   const namer = function () {
-//     const suffix = " the Llama"
-//     const name = possibleNames[randomizer]
-//   }
+let counter = 0;
+for (let k = 0; k < locations.length; k++) {
+  let invalidLocation = false;
+  const currentLocation = locations[k]
 
-//   return name + suffix
-// }
+  if (currentLocation[0] > 2) {
+    invalidLocation = true
+  } else {
+    counter++;
+  }
 
-// nameMaker = llamaNamer()
-// console.log(nameMaker())
+  if (invalidLocation) {
+    console.log("This location is invalid")
+  }
+}
+console.log(`There were ${counter} locations displayed`)
+
+
+// Lambda Llama
+const llamaNamer = function () {
+  const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+  const randomizer = Math.floor(Math.random() * 7)
+
+  const namer = function () {
+    const suffix = " the Llama"
+    const name = possibleNames[randomizer]
+    return name + suffix
+  }
+  return namer();
+}
+nameMaker = llamaNamer()
+console.log(nameMaker);
