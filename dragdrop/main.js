6e8c355d8d55bc7a1eb58f1b12711a687fe156d4
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-06-13 10:37:53 -0500

    basic specs fulfilled for drag and drop

--- dragdrop/main.js
+++ dragdrop/main.js
@@ -1,7 +1,9 @@
 // The user should be able to drag one of the middle cards into either the top box, or the bottom box.However, there's a problem with the way the code currently works. There's also a couple changes you need to make.
 
 // If you drag one of the cards into the top / bottom box, and then drag another card into the first one, you get a nested card.You need to prevent this from happening.
+
 // The user should only be able to drag one card into either box.Use the childNodes property to ensure that, if a card is already in the box, another can't be added.
+
 // The user should be able to move a card from the top / bottom box back to the middle.
 
 
@@ -27,16 +29,27 @@ const DragDropManager = Object.create(null, {
         target.ondrop = e => {
           // Enabled dropping on targets
           e.preventDefault()
-
+          console.log('event current target', e.currentTarget);
+          console.log('event  target', e.target);
           // Determine what's being dropped
           const data = e.dataTransfer.getData("text")
-
+          console.log('data?', data);
           // Append card to target component as child
-          // TODO: This should only happen if the target has no children nodes
-          // TODO: This should not happen if the target is another stage card
-          e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+          // This should only happen if the target has no children nodes
+          console.log('childnodes', target.childNodes);
+          // This should not happen if the target is another stage card
+          if (!target.childNodes.length) {
+            e.currentTarget.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+          }
         }
       })
+      const middle = document.querySelector('.origin');
+      middle.ondragover = e => e.preventDefault();
+      middle.ondrop = e => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.currentTarget.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+      }
     }
   }
 })
